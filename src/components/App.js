52d2a16a8922458531fa1de4baa7a073dd807822
Daniyar <dan1ar3110@gmail.com>
2019-12-10 21:52:37 +0600

    componentDidUpdate

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ export default class App extends Component{
     constructor() {
         super();
         this.state = {
-            selectedPerson: null
+            selectedPerson: 4
         }
     }
 
--- src/components/person-detail/ObjectDetail.js
+++ src/components/person-detail/ObjectDetail.js
@@ -6,33 +6,60 @@ export default class ObjectDetail extends Component{
     constructor(props) {
 
         super(props);
-
+        this.state = {
+            person: null
+        }
         this.swapiService = new SwapiService();
     }
+    componentDidMount() {
+        this.updatePerson();
+    }
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.props.personId !== prevProps.personId){
+            this.updatePerson();
+        }
+    }
 
+    updatePerson() {
+        const {personId} = this.props;
+        if(!personId){
+            return;
+        }
+
+        this.swapiService.getPerson(personId)
+            .then((person) => {
+                this.setState({
+                    person
+                })
+            })
+    }
 
     render() {
-
+            if(!this.state.person){
+                return <h3>Select person</h3>
+            }
+            console.log(this.state.person);
+            const {id,name,gender,birthYear,eyeColor} = this.state.person;
             return (
                 <div className="person-details card">
                     <img className="person-image"
-                         src={`https://starwars-visualguide.com/assets/img/characters/1.jpg`}/>
+                         src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}/>
 
                     <div className="card-body">
-                        <h4>name</h4>
+                        <h4>{name} {this.props.personId}</h4>
                         <ul className="list-group list-group-flush">
                             <li className="list-group-item">
                                 <span className="term">Gender</span>
-                                <span>gender</span>
+                                <span>{gender}</span>
                             </li>
                             <li className="list-group-item">
                                 <span className="term">Birth Year</span>
-                                <span>birthYear</span>
+                                <span>{birthYear}</span>
                             </li>
                             <li className="list-group-item">
                                 <span className="term">Eye Color</span>
-                                <span>eyeColor</span>
+                                <span>{eyeColor}</span>
                             </li>
                         </ul>
                     </div>
